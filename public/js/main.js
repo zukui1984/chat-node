7636e253b8000c41d869746c4bfa9307477c52e5
zukui <71074389+zukui1984@users.noreply.github.com>
2021-04-14 22:57:35 +0200

    add utils folder incl. messages.js and users.js + add botname & formatmessage

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,30 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 
+// Get username and room from URL
+const { username, room} = Qs.parse (location.search, {
+  ignoreQueryPrefix: true
+});
+
 
 const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 // Message from server
 socket.on('message', message => {
   console.log(message);
   outputMessage(message);
+
+// Scroll down
+chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message submit
@@ -19,15 +36,19 @@ chatForm.addEventListener('submit', e => {
 
   // Emit message to server
   socket.emit('chatMessage', msg);
+
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
 });
 
 // Output message to DOM
 function outputMessage(message) {
   const div = document.createElement('div');
   div.classList.add('message');
-  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
   <p class="text">
-   ${message}
+   ${message.test}
   </p>`;
   document.querySelector('.chat-messages').appendChild(div);
 }
